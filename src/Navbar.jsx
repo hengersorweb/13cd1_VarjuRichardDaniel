c8b442062999e52f9eb9f80461aa408e90182fac
Varju11 <varjuricharddaniel@gmail.com>
2025-02-17 11:58:20 +0100

    Router beállítás
    Menu rendszer kialakítása

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = (() =>{
 
     return(
         <div>
-            <h1>Az olimpia története</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus itaque neque deserunt optio numquam, dolorem minima fuga porro laudantium impedit nemo illo sequi illum necessitatibus qui, omnis quis animi quibusdam nesciunt. Vitae repellendus ipsa porro debitis, reiciendis quasi, magnam, deleniti adipisci repudiandae minus fugit molestias illo non repellat tempora voluptatem a sit unde recusandae nam sed natus distinctio architecto! Eligendi odio est commodi laborum? Error in pariatur modi impedit atque reiciendis minima voluptatem quod assumenda magnam totam debitis libero, vero dolore. Neque harum suscipit praesentium nemo debitis, itaque impedit ipsa illo sint asperiores, quod, minima dignissimos aut! Doloribus, aliquam amet.</p>
+            <nav>
+                <Link to='/'>Főoldal</Link>
+                <NavLink to='/ermesek'>Az olimpia érmesei</NavLink>
+                <NavLink to='/mermesek'>A magyar érmesek</NavLink>
+
+            </nav>
         </div>
     )
 
