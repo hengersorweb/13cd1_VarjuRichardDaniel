25186f919876a7d6b1db66d9e5b5d3bb32068958
Varju11 <varjuricharddaniel@gmail.com>
2025-02-17 11:29:47 +0100

    szükséges modulok importálása
    bootstrap, router, tachyons

---
+++ src/oldalak/Ermesek.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Ermesek = (() =>{
+
+    return(
+        <div>
+            <h1>Az olimpia érmesei</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus itaque neque deserunt optio numquam, dolorem minima fuga porro laudantium impedit nemo illo sequi illum necessitatibus qui, omnis quis animi quibusdam nesciunt. Vitae repellendus ipsa porro debitis, reiciendis quasi, magnam, deleniti adipisci repudiandae minus fugit molestias illo non repellat tempora voluptatem a sit unde recusandae nam sed natus distinctio architecto! Eligendi odio est commodi laborum? Error in pariatur modi impedit atque reiciendis minima voluptatem quod assumenda magnam totam debitis libero, vero dolore. Neque harum suscipit praesentium nemo debitis, itaque impedit ipsa illo sint asperiores, quod, minima dignissimos aut! Doloribus, aliquam amet.</p>
+        </div>
+    )
+
+}
+)
+export default Ermesek
