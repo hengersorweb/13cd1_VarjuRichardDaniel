1556d49d4cb21a75e5323476efc4f5bec66f07a1
Varju11 <varjuricharddaniel@gmail.com>
2025-02-17 12:15:11 +0100

    Menu elkészítése
    Css formázása

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,11 +5,10 @@ const Navbar = (() =>{
 
     return(
         <div>
-            <nav>
-                <Link to='/'>Főoldal</Link>
-                <NavLink to='/ermesek'>Az olimpia érmesei</NavLink>
-                <NavLink to='/mermesek'>A magyar érmesek</NavLink>
-
+            <nav className="bg-white black-80 tc pv4 avenir">
+                <Link className="f6 f5-l link bg-animate black-80 hover-bg-lightest-blue dib pa3 ph4-l" to='/'>Főoldal</Link>
+                <NavLink className="f6 f5-l link bg-animate black-80 hover-bg-light-green dib pa3 ph4-l" to='/ermesek'>Az olimpia érmesei</NavLink>
+                <NavLink className="f6 f5-l link bg-animate black-80 hover-bg-light-blue dib pa3 ph4-l" to='/mermesek'>A magyar érmesek</NavLink>
             </nav>
         </div>
     )
